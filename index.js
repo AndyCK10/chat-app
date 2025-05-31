9409773120e96403bbb6f6c8d39bc94f8ad55d76
Andy CK <ch10united@gmail.com>
2025-05-31 11:51:22 +0700

    feat: add sqlite db

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
-const express = require("express");
-const app = express();
 const path = require("path");
+const express = require('express');
 const http = require("http");
-const server = http.createServer(app);
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
 
-const { Server } = require("socket.io");
+const sqlite3 = require("sqlite3");
+const { open } = require("sqlite");
 
-// const io = new Server(server);
+async function main() {
+// open the database file
+const db = await open({
+  filename: "chat.db",
+  driver: sqlite3.Database,
+});
+
+// create our 'messages' table (you can ignore the 'client_offset' column for now)
+await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content TEXT
+    );
+  `);
+
+const app = express();
+const server = createServer(app);
 const io = new Server(server, {
-  connectionStateRecovery: {}
+  connectionStateRecovery: {},
 });
 
 app.get("/", (req, res) => {
-  // res.send('<h1>Hello BTD</h1>');
-  // req.sendFile(__dirname + '/index.html')
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
@@ -22,12 +39,55 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
-  socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
-    io.emit('chat message', msg);
+//   socket.on("chat message", (msg) => {
+//     console.log("message: " + msg);
+//     io.emit("chat message", msg);
+//   });
+  socket.on('chat message', async (msg, clientOffset, callback) => {
+    let result;
+    try {
+      result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', msg, clientOffset);
+    } catch (e) {
+      if (e.errno === 19 /* SQLITE_CONSTRAINT */ ) {
+        // the message was already inserted, so we notify the client
+        callback();
+      } else {
+        // nothing to do, just let the client retry
+      }
+      return;
+    }
+    io.emit('chat message', msg, result.lastID);
+    // acknowledge the event
+    callback();
   });
+
+  if (!socket.recovered) {
+    // if the connection state recovery was not successful
+    try {
+        db.each('SELECT id, content FROM messages WHERE id > ?',
+        [socket.handshake.auth.serverOffset || 0],
+        (_err, row) => {
+          socket.emit('chat message', row.content, row.id);
+        }
+      )
+    } catch (e) {
+      // something went wrong
+    }
+  }
+
+//   socket.on("request", (arg1, arg2, callback) => {
+//     console.log(arg1); // { foo: 'bar' }
+//     console.log(arg2); // 'baz'
+//     callback({
+//       status: "ok",
+//     });
+//   });
 });
 
 server.listen(3000, () => {
   console.log("listening on yport 3000");
 });
+
+}
+
+main();
